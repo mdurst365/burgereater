847b9605d95d3878a1b799cd45750df34dbfc8ae
Molly Durst <mollymadegraphics@yahoo.com>
2021-02-13 08:13:57 -0500

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Hamburger');
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
 INSERT INTO burgers (name) VALUES ('McRoyale with Cheese');
-INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Everything Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (name, devoured) VALUES ('Everything Burger', FALSE);
 INSERT INTO burgers (name) VALUES ('Pickle Burger');
