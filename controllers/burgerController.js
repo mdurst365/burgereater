22c597f213c4c2e3dd1e841f9549a5a881d9ace0
Molly Durst <mollymadegraphics@yahoo.com>
2021-02-13 10:44:44 -0500

    reworking burgerController file

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -5,57 +5,8 @@ var router = express.Router();
 var burger = require("../models/burgers.js");
 
 //creating the routes
-router.get('/', (req, res) => {
-  res.render('index');
-})
-
 router.get("/", (req, res) => {
-    burger.all(function (data) {
-        var hbsObject = {
-            burger: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
-    });
-});
-
-router.post("/api/burgers", (req, res) => {
-    burger.create([
-        "name", "devoured"
-    ], [
-        req.body.name, req.body.devoured
-    ], function (result) {
-        // Send back the ID of the new quote
-        res.json({ id: result.insertId });
-    });
-});
-
-router.put("/api/burgers/:id", (req, res) => {
-    const condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
-burger.update({
-    devoured: req.body.devoured
-}, condition, (result) => {
-    if (result.changedRows === 0) {
-        return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burger/:id", (req, res) => {
-    var condition = "id = " + req.params.id;
-  
-    cat.delete(condition, (result) => {
-      if (result.affectedRows == 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+      res.render("index");
   });
 
   //export for server.js
