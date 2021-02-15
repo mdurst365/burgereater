d67a2c5e67baa753478446105e4372d127e35a23
Molly Durst <mollymadegraphics@yahoo.com>
2021-02-15 05:54:01 -0500

    updated db files

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
 ### Schema
 
-DROP DATABASE burgers_db;
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
+
 USE burgers_db;
 
-CREATE TABLE burger
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT FALSE,
-	PRIMARY KEY (id)
+CREATE TABLE burger (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
 );
