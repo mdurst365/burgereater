fc728092377f9f96b7488eb6ee4a82a9f3b91d3e
Molly Durst <mollymadegraphics@yahoo.com>
2021-02-15 05:42:41 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-//express
 var express = require("express");
-var dotenv = require("dotenv").config();
 var PORT = process.env.PORT || 8080;
 var app = express();
+var dotenv = require("dotenv").config();
 
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//handlebars
-var expressHandlebars = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
-app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
 var routes = require("./controllers/burgerController.js");
 
 app.use(routes);
 
-//Check if the server is listening
+
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
 });
